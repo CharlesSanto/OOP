85a6857bf0334fb2ce96b3d001f1c1c1c9e7cdea
Charles Santos <coliveirasantos1234@gmail.com>
2025-08-09 10:37:21 -0300

    .

---
+++ Aluno.java
@@ -0,0 +1,26 @@
+public class Aluno 
+{
+    String nome;
+    int idade;
+    String curso;
+
+
+    public static void main(String[] args)
+    {
+        Aluno aluno1 = new Aluno();
+        aluno1.nome = "Charles";
+        aluno1.idade = 20;
+        aluno1.curso = "Ciência da Computação";
+
+        aluno1.Apresentar();
+    }
+
+    public void Apresentar()
+    {
+        System.out.println("\nApresentando aluno:\n");
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Idade: " + this.idade);
+        System.out.println("Curso: " + this.curso);
+        System.out.println("\n");
+    }
+}
